c141ea129c3d40825e47c003bc4646bf9005f914
Sujal Dhakal <85467296+Sectonic@users.noreply.github.com>
2023-08-23 13:06:18 -0600

    Update attendance.js

--- pages/dashboard/[meet_slug]/attendance.js
+++ pages/dashboard/[meet_slug]/attendance.js
@@ -119,7 +119,7 @@ export default function Attendance({ currentMeet }) {
                   <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
                 </label>
                 <OutsideClickHandler onOutsideClick={outsideHandler}>
-                  <ul tabIndex={0} className="flex-col dropdown-content menu p-2 shadow bg-base-100 rounded-box w-[280px] gap-2">
+                  <ul tabIndex={0} className="flex-col dropdown-content menu p-2 shadow bg-base-100 rounded-box w-[280px] gap-2 z-20">
                     <EventPicker 
                       currentMeet={currentMeet} 
                       setEvents={setEvents} 
