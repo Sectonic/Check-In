c57a534f8b4fc59e2dc1d78738229268e0c852c1
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-08-24 01:12:37 -0600

    solved issue (tardy still in db)

--- pages/api/get/meet.js
+++ pages/api/get/meet.js
@@ -2,13 +2,17 @@ import db from "@/lib/prisma";
 
 export default async function handler(req, res) {
     const { meetId } = req.query;
-    const meet = await db.meet.findFirst({
-        where: {
-            id: Number(meetId)
-        },
-        include: {
-            reoccurances: true
-        }
-    })
-    res.status(200).json({meet});
+    if (meetId === 'undefined') {
+        res.status(500).end();
+    } else {
+        const meet = await db.meet.findUnique({
+            where: {
+                id: Number(meetId)
+            },
+            include: {
+                reoccurances: true
+            }
+        })
+        res.status(200).json({meet});
+    }
 }
