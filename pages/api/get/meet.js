2302203278961c91768a5e556a1e2b389c3c6a89
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-08-24 01:09:37 -0600

    fix 3

--- pages/api/get/meet.js
+++ pages/api/get/meet.js
@@ -2,7 +2,7 @@ import db from "@/lib/prisma";
 
 export default async function handler(req, res) {
     const { meetId } = req.query;
-    const meet = await db.meet.findUnique({
+    const meet = await db.meet.findFirst({
         where: {
             id: Number(meetId)
         },
