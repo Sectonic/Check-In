08104f3dbab91db6de80ee70cf6aeb6da13f3554
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-08-24 01:01:40 -0600

    fix meet 2

--- pages/api/get/meet.js
+++ pages/api/get/meet.js
@@ -2,17 +2,13 @@ import db from "@/lib/prisma";
 
 export default async function handler(req, res) {
     const { meetId } = req.query;
-    if (!meetId) {
-        res.status(500).end();
-    } else {
-        const meet = await db.meet.findUnique({
-            where: {
-                id: Number(meetId)
-            },
-            include: {
-                reoccurances: true
-            }
-        })
-        res.status(200).json({meet});
-    }
+    const meet = await db.meet.findUnique({
+        where: {
+            id: Number(meetId)
+        },
+        include: {
+            reoccurances: true
+        }
+    })
+    res.status(200).json({meet});
 }
