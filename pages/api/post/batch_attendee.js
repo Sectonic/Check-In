5c74ba5fc33fa450b1275f0695f17e7a5ee13f06
Sujal Dhakal <85467296+Sectonic@users.noreply.github.com>
2023-08-29 12:04:44 -0600

    Update batch_attendee.js

--- pages/api/post/batch_attendee.js
+++ pages/api/post/batch_attendee.js
@@ -15,48 +15,25 @@ export default ApiRoute(
         }
         const { newAttendees, previousAttendees } = req.body;
 
-        const allSpecificIds = await db.attendee.findMany({
-            where: {
-                organizer: {
-                    id: user.id
-                }
-            },
-            select: {
-                specificId: true
-            }
-        });
-
-        const savingIds = [];
+        await db.attendee.deleteMany({ where: { organizer: { id: user.id } } });
 
         for (var i = 0; i < newAttendees.length; i++) {
             const { name, id } = newAttendees[i];
-            if (!previousAttendees.some(prev => prev.name === name && prev.specificId === id)) {
-                var checkedNewId = false;
-                var newId = id;
-                while (!id && !checkedNewId) {
-                    newId = randomString(10);
-                    if (!allSpecificIds.includes(newId)) {
-                        checkedNewId = true;
-                    }
+            var checkedNewId = false;
+            var newId = id;
+            while (!id && !checkedNewId) {
+                newId = randomString(10);
+                if (!allSpecificIds.includes(newId)) {
+                    checkedNewId = true;
                 }
-                savingIds.push(newId);
-                await db.attendee.create({
-                    data: {
-                       name, specificId: newId,
-                       organizer: { connect: { id: user.id } } 
-                    }
-                });
-            } else {
-                savingIds.push(id);
             }
+            await db.attendee.create({
+                data: {
+                   name, specificId: newId,
+                   organizer: { connect: { id: user.id } } 
+                }
+            });
         }
-
-        await db.attendee.deleteMany({
-            where: {
-                specificId: { notIn: savingIds },
-                organizer: { id: user.id }
-            }
-        })
         
         res.status(200).send({message: 'Attendance successfully recorded'});
     }
